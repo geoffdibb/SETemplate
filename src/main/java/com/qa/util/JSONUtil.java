3c13e547081fa4b501f08730c590edc526eabc71
Marcofdoom <marcvpartington@gmail.com>
2019-06-05 16:16:38 +0100

    Implemented columns

---
+++ src/main/java/com/qa/util/JSONUtil.java
@@ -0,0 +1,48 @@
+package com.qa.util;
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JSONUtil {
+
+	private ObjectMapper json;
+
+	public JSONUtil() {
+		this.json = new ObjectMapper();
+	}
+
+	public String getJSONForObject(Object obj) {
+		try {
+			return json.writeValueAsString(obj);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public <T> T getObjectForJSON(String jsonString, Class<T> clazz) {
+
+		try {
+			return json.readValue(jsonString, clazz);
+
+		} catch (JsonParseException e) {
+
+			e.printStackTrace();
+
+		} catch (JsonMappingException e) {
+
+			e.printStackTrace();
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		}
+		return null;
+	}
+
+}
