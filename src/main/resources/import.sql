035f7291cacd7169b2cacd7b7270b07396832c2e
Marcofdoom <marcvpartington@gmail.com>
2019-06-05 16:36:05 +0100

    Altered caps for sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,3 @@
-INSERT INTO MOVIE (Id, accountNumber, firstName, lastName) VALUES (1234, 1234, 'John', 'Doe')
-INSERT INTO MOVIE (Id, accountNumber, firstName, lastName) VALUES (1235, 1235, 'Jane', 'Doe')
-INSERT INTO MOVIE (Id, accountNumber, firstName, lastName) VALUES (1236, 1236, 'Jim', 'Tyler')
+INSERT INTO ACCOUNT (Id, accountNumber, firstName, lastName) VALUES (1234, 1234, 'John', 'Doe')
+INSERT INTO ACCOUNT (Id, accountNumber, firstName, lastName) VALUES (1235, 1235, 'Jane', 'Doe')
+INSERT INTO ACCOUNT (Id, accountNumber, firstName, lastName) VALUES (1236, 1236, 'Jim', 'Tyler')
